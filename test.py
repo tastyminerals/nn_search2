2a1f7290a0901b5c5c8a2daaaddf5d7de0bd6f64
tastyminerals <tastyminerals@gmail.com>
2016-02-08 01:43:40 +0100

    clean up, added test.py with Query match draft

--- test.py
+++ test.py
@@ -1,26 +1,53 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-
 sent = {0: [('a', 'NNS', 0), ('b', 'VB', 1), ('c', 'NN', 2),
             ('d', 'DT', 3), ('e', 'NN', 4), ('f', 'JJ', 5)]}
 
+query = (['a', 'NNS', None], [None, 'NN', 3])
 
-query = (['c', 'NN', None], [None, 'NN', 5])
-
-matched = []
-for q in query:
-    print 'Iterating', q
-    for node in sent[0][start:]
-        print 'Checking', q, node
-        if q[0] in node or q[1] in node:
-            print 'q[0] or q[1] in node', True
-            if q[2] and q[2] >= node[-1]:
-                print 'q[2] > node[-1]', True
-                matched.append(node)
-                print 'Breaking'
+def single_match(query, sent):
+    prev = 0
+    matched = []
+    for q in query:
+        words_before = 0
+        print 'Iterating', q
+        for node in sent[0][prev:]:
+            words_before += 1
+            if q[2] and q[2] + 1 < words_before:
                 break
+            print 'Checking', q, '==', node
+            if q[0]:
+                print 'Matching', q[0], node
+                if q[0] in node:
+                    print 'MATCHED!'
+                else:
+                    print 'NOT MATCHED!'
+                    continue
 
-print matched
+            if q[1]:
+                print 'Matching', q[1], node
+                if q[1] in node:
+                    print 'MATCHED!'
+                else:
+                    print 'NOT MATCHED!'
+                    continue
 
+            if isinstance(q[2], int):
+                print 'Matching', q[2] + node[-1], node[-1]
+                if q[2] + node[-1] > node[-1]:
+                    print 'MATCHED!'
+                    matched.append(node)
+                    prev = node[-1] + 1
+                    break
+                else:
+                    print 'NOT MATCHED!'
+                    continue
 
+            matched.append(node)
+            prev = node[-1] + 1
+            break
+    print matched
+    print words_before
+
+single_match(query, sent)
 
